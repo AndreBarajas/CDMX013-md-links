6b0363f83500077d6672eb0045f26ca510834682
Andrea Barajas <108833574+AndreBarajas@users.noreply.github.com>
2022-10-27 18:15:53 -0500

    return an object with status request

--- bin/examples/readmeeee.md
+++ bin/examples/readmeeee.md
@@ -1,4 +1,3 @@
-# PRUEBA 
 ## 1. Preámbulo
 
 [Markdown](https://es.wikipedia.org/wiki/Markdown) es un lenguaje de marcado
--- bin/recursive-dir.js
+++ bin/recursive-dir.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const color = require('colors');
 const fs = require('fs');
 const path = require('path');
@@ -21,7 +22,7 @@ const path = require('path');
 // console.log('resultado ruta abs', resultRoute);
 
 // // Return array with only .md files 
-function getFileMd(absolutePath) {
+function getFilesMd(absolutePath) {
     console.log('path usuario', path)
     if (fs.lstatSync(absolutePath).isFile()) {
         if (path.extname(absolutePath) === '.md') {
@@ -32,26 +33,79 @@ function getFileMd(absolutePath) {
         }       
     } else {
         const results = fs.readdirSync(absolutePath);
-        const innerFolders = results.map(result => getFileMd(absolutePath + '\\' + result));
+        const innerFolders = results.map(result => getFilesMd(absolutePath + '\\' + result));
         const oneArray = innerFolders.flat(Infinity);
         // console.log("flat array", oneArray);
         return oneArray;
     }
 }
-const resultLoop = getFileMd('C:\\Users\\abaja\\Documents\\Laboratoria\\CDMX013-md-links\\bin');
-console.log('variable resultLoop', resultLoop);
+const resultMds = getFilesMd('C:\\Users\\abaja\\Documents\\Laboratoria\\CDMX013-md-links\\bin');
+// console.log('variable resultLoop', resultMds);
 
 
 // Read files .md
-function readFile (pathsFiles) {
-    const contentFile = pathsFiles.forEach(file => fs.readFileSync(file, 'utf-8'));
-    console.log("lectura files", contentFile);
-    return contentFile;
+function readFile(pathsFiles) {
+    let arrayRead = []; 
+    pathsFiles.forEach(file => { 
+        const content = fs.readFileSync(file, 'utf-8'); 
+        const regEx = /\[(.+)\]\((https?:\/\/.+)\)/gi;
+        const filter = [...content.matchAll(regEx)];
+        filter.forEach(res => {
+             let contentFile = {
+                href: res[2],
+                text: res[1],
+                file: file,
+             };
+             arrayRead.push(contentFile);
+        })        
+    })
+    return arrayRead;
+ };
+
+const resultReadFile = readFile (resultMds);
+// console.log("resultado de leer los archivos", resultReadFile);
+
+// Validate links
+const getStatus = (arr) => {
+    let statusLinks = [];
+    return new Promise((resolve, reject) => {
+        axios({
+            url: arr.href
+        }).then((content) => {
+            statusLinks.push({
+                href: arr.href,
+                text: arr.text,
+                file: arr.file,
+                status: content.status + content.statusText,
+            });
+            console.log("statusLinks", statusLinks);
+            resolve({
+                statusLinks
+            })
+        }).catch(() =>  {
+            statusLinks.push({
+                href: arr.href,
+                text: arr.text,
+                file: arr.file,
+                status: '404',
+            });
+            console.log(statusLinks);
+                resolve({
+                   statusLinks
+                })
+            })
+    })
 };
+    
 
-const resultReadFile = readFile (resultLoop);
-console.log("resultado de leer los rachivos", resultReadFile);
-
+// Promise All
+Promise.all([getStatus({
+    href: 'https://nodejs.org/',
+    text: 'Node.js',
+    file: 'C:\\Users\\abaja\\Documents\\Laboratoria\\CDMX013-md-links\\bin\\examples\\readmeeee.md'
+})])
+    .then(response => console.log("promise all", response)
+    );
 
     
 
